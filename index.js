34a72a2e928d56cf4f3825938c7b1de66d4e3fc2
jonikumardas <jonikumardas213@gmail.com>
2022-05-09 19:14:19 +0600

    create mathod end

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// meadele were 
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASS}@cluster0.7qpx0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const carProduct = client.db("user").collection("car");
+        console.log("db is connected");
+        app.post('/upload', async (req, res) => {
+            const product = req.body;
+            const result = await carProduct.insertOne(product);
+            res.send(result);
+            console.log(product);
+        })
+
+        
+        
+
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('backend connected')
@@ -9,3 +40,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// car
+// UBYyfyAGMcMh1jEB
+
+// 
+
+// 
